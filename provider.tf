a13ef167a6e7ad499e60373956e1f2c8819868c7
Reddylab36 <shekharreddy36@outlook.com>
2024-09-02 11:30:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,23 @@
-pipeline
+pipeline{
+    agent any
+    tools {
+        terraform 'terraform-1.9.5'
+    }
+    stages{
+        stage("git checkout"){
+            steps{
+                git branch: 'main', changelog: false, poll: false, url: 'https://github.com/Reddylab36/terraform-test'
+            }
+        }
+        stage("terraform init"){
+            steps{
+                sh 'terraform init'
+            }
+        }
+        stage("terraform apply"){
+            steps{
+                sh 'terraform apply --auto-approve'
+            }
+        }
+    }
+}
