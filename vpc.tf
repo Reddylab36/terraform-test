17c0914f52a30848b0b229ea17b2ae2efc9c2cef
Reddylab36 <shekharreddy36@outlook.com>
2024-09-02 16:58:22 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0a0e5d9c7acc336f1" # us-eat-1
   instance_type = "t2.micro"  
 }
