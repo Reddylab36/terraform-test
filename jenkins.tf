4f2317ddea1760a2f6062543fb65f1d57deb6b45
Reddylab36 <shekharreddy36@outlook.com>
2024-09-03 16:53:09 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -1,23 +1,22 @@
-pipeline{
+pipeline {
     agent any
-    tools {
-        terraform "terraform-1.9.5"
-    }
-    stages{
-        stage("git checkout"){
-            steps{
-                git branch: 'main', changelog: false, poll: false, url: "https://github.com/Reddylab36/terraform-test.git"
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Reddylab36/terraform-test/'
             }
         }
-        stage("terraform init"){
-            steps{
+        stage('Terraform init') {
+            steps {
                 sh 'terraform init'
             }
         }
-        stage("terraform apply"){
-            steps{
+        stage('Terraform apply') {
+            steps {
                 sh 'terraform apply --auto-approve'
             }
         }
+        
     }
 }
